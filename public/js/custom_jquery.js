f6c57c6b862ae6480d6a3e0fb22239c24903f98b
Ferenc Béres <ferdzso05@gmail.com>
2016-05-29 13:51:24 +0200

    replace html hrefs with jquery calls

--- public/js/custom_jquery.js
+++ public/js/custom_jquery.js
@@ -1,7 +1,40 @@
 $(document).ready(function(){
 
-$('.table > tbody > tr').click(function() {
+$('.event-item').click(function() {
     window.open($(this).attr('href'));
+});
+
+
+// updting events
+
+$('.to-trash-btn').click(function(e) {
+	event_id = $(this).parent().parent().attr('id');
+	window.location.replace('update/' + event_id + '/move/trash');
+	e.stopPropagation();
+});
+
+$('.to-local-btn').click(function(e) {
+	event_id = $(this).parent().parent().attr('id');
+	window.location.replace('update/' + event_id + '/move/local');
+	e.stopPropagation();
+});
+
+$('.to-new-btn').click(function(e) {
+	event_id = $(this).parent().parent().attr('id');
+	window.location.replace('update/' + event_id + '/move/new');
+	e.stopPropagation();
+});
+
+$('.reset-btn').click(function(e) {
+	event_id = $(this).parent().parent().attr('id');
+	window.location.replace('update/' + event_id + '/reset');
+	e.stopPropagation();
+});
+
+$('.remove-btn').click(function(e) {
+	event_id = $(this).parent().parent().attr('id');
+	window.location.replace('update/' + event_id + '/remove');
+	e.stopPropagation();
 });
 
 });
